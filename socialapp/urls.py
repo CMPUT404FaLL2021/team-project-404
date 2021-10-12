54f82b1ab783b808bddac08862bceb41ac8193d8
Hao Liu <liu9@ualberta.ca>
2021-10-12 02:14:29 -0600

    show and edit post

--- socialapp/urls.py
+++ socialapp/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path('login/', views.login, name = 'login'),
     path('register/', views.register, name = 'register'),
     path('logout/', views.logout, name = 'logout'),
-    path('add_post/', views.add_post, name = 'add_post')
+    path('add_post/', views.add_post, name = 'add_post'),
+    path('edit_post/', views.edit_post, name = 'edit_post'),
+    path('show_post/', views.show_post, name = 'show_post')
 ]
