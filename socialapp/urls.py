ae15fc40827333cf642e0ee5d3e5eec44ba04acf
WeiyiWu <wwu3@ualberta.ca>
2021-10-12 10:52:12 +0800

    add_post

--- socialapp/urls.py
+++ socialapp/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('login/', views.login, name = 'login'),
     path('register/', views.register, name = 'register'),
     path('logout/', views.logout, name = 'logout'),
+    path('add_post/', views.add_post, name = 'add_post')
 ]
