f252f646628f549f71a7cd93a4b36bad39bc3f83
zhengyao603 <zhengyao@ualberta.ca>
2021-10-16 01:15:01 -0600

    update urls

--- socialapp/urls.py
+++ socialapp/urls.py
@@ -10,8 +10,8 @@ urlpatterns = [
     path('register/', views.register, name = 'register'),
     path('logout/', views.logout, name = 'logout'),
     path('add_post/', views.add_post, name = 'add_post'),
-    path('edit_post/', views.edit_post, name = 'edit_post'),
-    path('show_post/', views.show_post, name = 'show_post'),
-    path('user_profile/', views.user_profile, name = 'user_profile'),
+    path('edit_post/<int:edit_post_id>/', views.edit_post, name = 'edit_post'),
+    path('show_post/<int:show_post_id>/', views.show_post, name = 'show_post'),
+    path('user_profile/<str:username>/', views.user_profile, name = 'user_profile'),
     path('mainPage/', views.mainPage, name = 'mainPage')
 ]
