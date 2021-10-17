747ae982b06e3e0102e6a516e688dbf6d66a829f
zhengyao603 <zhengyao@ualberta.ca>
2021-10-17 03:40:19 -0600

    fix url bugs

--- socialapp/urls.py
+++ socialapp/urls.py
@@ -9,9 +9,9 @@ urlpatterns = [
     path('login/', views.login, name = 'login'),
     path('register/', views.register, name = 'register'),
     path('logout/', views.logout, name = 'logout'),
-    path('add_post/', views.add_post, name = 'add_post'),
-    path('edit_post/<int:edit_post_id>/', views.edit_post, name = 'edit_post'),
-    path('show_post/<int:show_post_id>/', views.show_post, name = 'show_post'),
+    path('add_post/<str:username>/', views.add_post, name = 'add_post'),
+    path('edit_post/<str:username>/<int:edit_post_id>/', views.edit_post, name = 'edit_post'),
+    path('show_post/<str:username>/<int:show_post_id>/', views.show_post, name = 'show_post'),
     path('user_profile/<str:username>/', views.user_profile, name = 'user_profile'),
     path('mainPage/<str:username>/', views.mainPage, name = 'mainPage')
 ]
