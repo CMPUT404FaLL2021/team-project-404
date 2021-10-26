bd2da2f96f96079fb5033133417a5017b9a341a8
WeiyiWu <wwu3@ualberta.ca>
2021-10-26 23:25:43 +0800

    added api methods & fixed delete comment

--- socialapp/api/serializers.py
+++ socialapp/api/serializers.py
@@ -1,7 +1,41 @@
 from rest_framework import serializers
-from socialapp.models import Post
+from socialapp.models import Author, Post, Comment
+
+class AuthorSerializer(serializers.ModelSerializer):
+    type = serializers.SerializerMethodField()
+    @classmethod
+    def get_type(self, object):
+        return 'author'
+    class Meta:
+        model = Author
+        fields = ['type', 'id', 'displayName']
+
+class CommentSerializer(serializers.ModelSerializer):
+    type = serializers.SerializerMethodField()
+    author = AuthorSerializer(read_only=True)
+    @classmethod
+    def get_type(self, object):
+        return 'comment'
+    class Meta:
+        model = Comment
+        fields = ['type', 'author', 'comment', 'contentType', 'published', 'id']
 
 class PostSerializer(serializers.ModelSerializer): 
+    type = serializers.SerializerMethodField()
+    author = AuthorSerializer(read_only=True)
+    count = serializers.SerializerMethodField()
+    comment = serializers.SerializerMethodField()
+    @classmethod
+    def get_type(self, object):
+        return 'post'
+    
+    def get_count(self, object):
+        return Comment.objects.filter(post=object).count()
+
+    def get_comment(self, object):
+        return CommentSerializer(Comment.objects.filter(post=object), many=True).data
+
     class Meta:
         model = Post
-        fields = ['title', 'description', 'contentType', 'content', 'author', 'published', 'visibility', 'unlisted', 'id']
+        fields = ['type', 'title', 'id', 'description', 'contentType', 'content', 'author', 'count', 'comment', 'published', 'visibility', 'unlisted']
+
--- socialapp/api/views.py
+++ socialapp/api/views.py
@@ -6,20 +6,53 @@ from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 
-from socialapp.models import Post, Author
-from socialapp.api.serializers import PostSerializer
+from socialapp.models import Author, Post, Comment
+from socialapp.api.serializers import AuthorSerializer, PostSerializer, CommentSerializer
 
 @api_view(['GET', 'POST'])
+def api_author_detail(request, author_id):
+    try:
+        author = Author.objects.get(id=author_id)
+    except Author.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = AuthorSerializer(author)
+        return Response(serializer.data)
+
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+def api_post_comments(request, author_id, post_id):
+    try:
+        comments = Comment.objects.filter(post=post_id)
+    except Comment.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    # GET get comments of the post
+    if request.method == 'GET':
+        serializer = CommentSerializer(comments, many=True)
+        return Response(serializer.data)
+
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+@api_view(['GET', 'POST', 'DELETE'])
 def api_post_detail(request, author_id, post_id):
     try:
         post = Post.objects.get(id=post_id)
     except Post.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
 
+    # GET get the public post
     if request.method == 'GET':
         serializer = PostSerializer(post)
         return Response(serializer.data)
 
+    # POST update the post (must be authenticated)
+    # missing authenticating step
     elif request.method == 'POST':
         data = JSONParser().parse(request)
         data['id'] = post_id
@@ -29,3 +62,31 @@ def api_post_detail(request, author_id, post_id):
             data['success'] = 'Updated Successfully'
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    # DELETE remove the post
+    elif request.method == 'DELETE':
+        operation = post.delete()
+        data = {}
+        if operation:
+            data['success'] = "Delete successfully"
+        else:
+            data['failure'] = "Delete failed"
+        return Response(data=data)
+
+    # PUT create a post with that post_id
+
+@api_view(['GET'])
+def api_post_like(request, author_id, post_id):
+    try:
+        post = Post.objects.get(id=post_id)
+        author = Author.objects.get(id=author_id)
+    except Post.DoesNotExist or Author.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    data = {}
+    if request.method == 'GET':
+        data['summary'] = author.displayName + 'Likes your post'
+        data['type'] = 'like'
+        data['author'] = AuthorSerializer(author).data
+        data['object'] = post_id
+        return Response(data=data)
