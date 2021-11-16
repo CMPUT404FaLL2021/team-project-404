68c5ec6be2fdc97831d8aedf4c79eed6cfb54e26
zhengyao603 <zhengyao@ualberta.ca>
2021-11-16 07:15:22 -0700

    update authors api

--- socialapp/api/urls.py
+++ socialapp/api/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from socialapp.api import views
 
 urlpatterns = [
+    path('authors/', views.api_authors_profile, name='authors_profile'),
     path('author/<uuid:author_id>/', views.api_author_detail, name='author_detail'),
     path('author/<uuid:author_id>/posts/<uuid:post_id>/comments/', views.api_post_comments, name='comments_detail'),
     path('author/<uuid:author_id>/posts/<uuid:post_id>/likes/', views.api_post_like, name='post_like'),
